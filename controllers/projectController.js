07a72fda9d8fa14bfaead592632e1d755e7ef752
LokiOfRagnar <aad@almende.org>
2018-10-31 14:37:33 +0100

    initial commit, locally

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -26,3 +26,26 @@ exports.project_details = function(rcn, list){
     });
 }
 
+// get a list of contributors from one country with a reference to a project
+exports.contributions_by_country= function(countryid, list){
+    console.log('country: '+countryid);
+    let contributions = {};
+    list.forEach(function(project, index){
+        project.organizations.forEach(function(org, index){
+            if(org.country.toUpperCase() === countryid.toUpperCase()){
+                contributions.add({org: org, project: project});
+            }
+        });
+    });
+    return contributions.sort(function(contributionA, contributionB){
+        let result = contributionA.org.name.localeCompare(contributionB.org.name);
+        if(!result){
+            // the it is the same organization, sort on project name
+            return contributionA.project.name.localeCompare(contributionB.project.name);
+        }
+        return result;
+    })
+    // At this point a list of organizations has been created from the indicated country with a bunch of projects
+    return contributions;
+}
+
